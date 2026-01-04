0d1952f6c6621ec96e46e955c6c945c83c6ad7c5
Concept Solutions <aitsunomanweb@gmail.com>
2026-01-04 13:00:37 +0530

    Update __manifest__.py

--- concept_amc_and_renew_management/__manifest__.py
+++ concept_amc_and_renew_management/__manifest__.py
@@ -1,35 +1,50 @@
 {
     'name': 'AMC & Renewal Management',
     'version': '1.0.0',
-    'category': 'AMC & Renewal Management',
+    'category': 'Sales/CRM',
     'summary': 'Manage Annual Maintenance Contracts (AMC) and Renewals',
     'author': 'Concept Solutions LLC',
     'company': 'Concept Solutions LLC',
+    'website': 'https://www.csloman.com',
+    'maintainer': 'Concept Solutions LLC',
     'description': """
         AMC Management module helps you:
         - Manage AMCs
         - Auto generate quotations
         - Integrate with CRM and Sales
         """,
-    'maintainer': 'Concept Solutions LLC',
-    'website': 'https://www.csloman.com',
-    'depends': ['base','project','mail','crm','sale_management','stock','account','sale_crm','account','sale'],
+    'depends': [
+        'base',
+        'project',
+        'mail',
+        'crm',
+        'sale_management',
+        'stock',
+        'account',
+        'sale_crm',
+        'sale'
+    ],
     'data': [
-            'views/amc_dashboard.xml',
-            'views/sale_views.xml',
-            'views/crm_views.xml',
-            'wizard/amc_link_wizard.xml',
-            'views/menus.xml',
-            'views/account_move_view.xml',
-            'security/ir.model.access.csv',
-            'data/ir_cron_data.xml',
+        'security/ir.model.access.csv',
+        'data/ir_cron_data.xml',
+        'views/amc_dashboard.xml',
+        'views/sale_views.xml',
+        'views/crm_views.xml',
+        'views/account_move_view.xml',
+        'wizard/amc_link_wizard.xml',
+        'views/menus.xml',
     ],
     'assets': {
         'web.assets_backend': [
             'concept_amc_and_renew_management/static/src/css/amc_hide_label.css',
         ],
     },
-    'assets': {},
-    'installable': True, 
+    # Store Configuration
+    'price': 110.00,
+    'currency': 'USD',
+    'license': 'OPL-1',
+    'images': ['static/description/banner.png'], # Recommended: Ensure you have a banner
+    'installable': True,
     'auto_install': False,
+    'application': True,
 }
